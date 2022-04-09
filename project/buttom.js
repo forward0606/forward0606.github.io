65d9b0b151f85ce455ca1c07156590321a6132d9
forward0606 <73343386+forward0606@users.noreply.github.com>
2022-04-10 01:02:10 +0800

    Update buttom.js

--- project/buttom.js
+++ project/buttom.js
@@ -27,5 +27,7 @@ start_stop.onclick = function () {
         time.innerHTML = left_time;
         count_down = setInterval(timer, 1000);
         start_stop.innerHTML = "STUDY";
+    }else{
+        start_stop.innerHTML = "START";   
     }
 }
