99ac3117006142b933ee3fe5a1d4c2f53cc6878f
forward0606 <73343386+forward0606@users.noreply.github.com>
2022-04-10 14:00:34 +0800

    Update buttom.js

--- project/buttom.js
+++ project/buttom.js
@@ -1,13 +1,16 @@
 
 const start_stop = document.querySelector("#start_stop");
-const time = document.querySelector("#time");
+const minute = document.querySelector("#minute");
+const second = document.querySelector("#second");
 
-let count_down = null, count_down_from = 10;
+let count_down = null, count_down_from = parseInt(minute.innerHTML, 10) * 60 + parseInt(second.innerHTML, 10);
 let left_time = count_down_from;
 
 function timer() {
     left_time -= 1;
-    time.innerHTML = left_time;
+    let tmp = Math.floor(left_time / 60);
+    minute.innerHTML = (tmp < 10 ? '0' + tmp : tmp);
+    second.innerHTML = (left_time % 60 < 10 ? '0' + left_time % 60 : left_time % 60);
     if (left_time == 0) {
         clearInterval(count_down);
         count_down = null;
@@ -24,10 +27,14 @@ function timer() {
 
 start_stop.onclick = function () {
     if (start_stop.innerHTML === "START") {
-        time.innerHTML = left_time;
+        let tmp = Math.floor(left_time / 60);
+        minute.innerHTML = (tmp < 10 ? '0' + tmp : tmp);
+        second.innerHTML = (left_time % 60 < 10 ? '0' + left_time % 60 : left_time % 60);
         count_down = setInterval(timer, 1000);
-        start_stop.innerHTML = "STUDY";
-    }else{
-        start_stop.innerHTML = "START";   
+        start_stop.innerHTML = "STOP";
+    } else {
+        start_stop.innerHTML = "START";
+        clearInterval(count_down);
+        count_down = null;
     }
 }
